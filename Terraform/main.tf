8fa5d2ff6f83da4fb067aa00f2ea8e584345390a
WarpWing <professornitro@gmail.com>
2021-12-18 23:51:53 -0500

    Complete v1 of desired state

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -7,17 +7,50 @@ terraform {
   }
 }
 
+provider "lxd" {
+  generate_client_certificates = true
+  accept_remote_certificate    = true
+} 
 resource "lxd_container" "anvil" {
   name      = "anvil"
   image     = "ubuntu:20.04"
   ephemeral = false
+  profiles = ["default"]
+  limits     = {
+      "memory" = "2800MB"
+      "cpu" = 2
+  }
+}
 
+resource "lxd_container" "metal1" {
+  name      = "metal1"
+  image     = "ubuntu:20.04"
+  ephemeral = false
+  profiles = ["default"]
+   limits     = {
+      "memory" = "1800MB"
+      "cpu" = 2
+  }
+}
+
+resource "lxd_storage_pool" "bucket" {
+  name = "bucket"
+  driver = "zfs"
   config = {
-    "boot.autostart" = true
+    size   = "40GB"
   }
+}
 
-  limits = {
-    cpu = 1
-    memory = 2
-  }
+resource "lxd_volume" "forgechild" {
+  name = "forgechild"
+  pool = "${lxd_storage_pool.bucket.name}"
+}
+
+
+output "anvil-ip" {
+  value = lxd_container.anvil.ip_address
+}
+
+output "metal1-ip" {
+  value = lxd_container.metal1.ip_address
 }
