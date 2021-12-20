4637a34fa1f58282065e9f2d2a88dcef657aac36
WarpWing <professornitro@gmail.com>
2021-12-20 12:11:22 -0500

    Basic auth

---
+++ SeedVault/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI, Request, HTTPException
+import json
+app = FastAPI()
+# uvicorn main:app --reload
+authkey = "Pc5yM5ebmfs3Cv7" # Temp Auth Key
+
+@app.get("/register/{user}/{hostname}/{key}")
+def read_root(key: str,hostname: str,user: str, request: Request):
+    client_host = request.client.host
+    print(key)
+    if key != authkey: 
+        raise HTTPException(status_code=404, detail="Invalid key.")
+    else: 
+        return {"client_host": client_host, "hostname": hostname, "user": user, "key": key}
