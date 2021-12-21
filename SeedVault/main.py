95b4efda59c7db3bab8878af1e37d62479f7f715
WarpWing <professornitro@gmail.com>
2021-12-20 19:08:04 -0500

    Destroy print

--- SeedVault/main.py
+++ SeedVault/main.py
@@ -7,7 +7,14 @@ authkey = "Pc5yM5ebmfs3Cv7" # Temp Auth Key
 @app.get("/register/{user}/{hostname}/{key}")
 def read_root(key: str,hostname: str,user: str, request: Request):
     client_host = request.client.host
-    print(key)
+    if key != authkey: 
+        raise HTTPException(status_code=404, detail="Invalid key.")
+    else: 
+        return {"client_host": client_host, "hostname": hostname, "user": user, "key": key}
+
+@app.get("/destroy/{user}/{hostname}/{key}")
+def read_root(key: str,hostname: str,user: str, request: Request):
+    client_host = request.client.host
     if key != authkey: 
         raise HTTPException(status_code=404, detail="Invalid key.")
     else: 
